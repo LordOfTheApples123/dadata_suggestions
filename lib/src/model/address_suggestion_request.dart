45a510daa17193858394106ffe7e9c69a226bcc9
AntonovAleksandr <zzzz1917@bk.ru>
2023-04-14 16:54:54 +0300

    boundary correction

--- lib/src/model/address_suggestion_request.dart
+++ lib/src/model/address_suggestion_request.dart
@@ -4,6 +4,7 @@ import 'address_suggestion_constraint.dart';
 import 'address_suggestion_priority.dart';
 import 'address_suggestion_radius_constraint.dart';
 import 'level_boundry.dart';
+
 part 'address_suggestion_request.g.dart';
 
 ///Used to call address sugestions API.
@@ -31,38 +32,23 @@ class AddressSuggestionRequest {
   LevelBoundry _lowerBoundary;
 
   @JsonKey(name: 'from_bound')
-  String get upperBoundary {
-    return _upperBoundary.value;
+  LevelBoundry get upperBoundary {
+    return _upperBoundary;
   }
 
   @JsonKey(name: 'from_bound')
-  set upperBoundary(dynamic value) {
-    if (value is String) {
-      _upperBoundary = LevelBoundryValue.forValue(value);
-      return;
-    }
-    if (value is LevelBoundry) {
-      _upperBoundary = value;
-      return;
-    }
-    _upperBoundary = null;
+  set upperBoundary(LevelBoundry value) {
+    _upperBoundary = value;
   }
 
   @JsonKey(name: 'to_bound')
-  String get lowerBoundary {
-    return _lowerBoundary.value;
+  LevelBoundry get lowerBoundary {
+    return _lowerBoundary;
   }
 
   @JsonKey(name: 'to_bound')
-  set lowerBoundary(dynamic value) {
-    if (value is String) {
-      _lowerBoundary = LevelBoundryValue.forValue(value);
-      return;
-    }
-    if (value is LevelBoundry) {
-      _lowerBoundary = value;
-    }
-    _lowerBoundary = null;
+  set lowerBoundary(LevelBoundry value) {
+    _lowerBoundary = value;
   }
 
   ///AddressSuggestionRequest represents an serializable object
--- lib/src/model/level_boundry.dart
+++ lib/src/model/level_boundry.dart
@@ -1,65 +1,20 @@
+import 'package:json_annotation/json_annotation.dart';
+
 ///Levels of `from_bound` and `to_bound` according to
 ///Dadata online API documentation https://confluence.hflabs.ru/pages/viewpage.action?pageId=285343795.
-enum LevelBoundry { country, region, area, city, settlement, street, house }
-
-/// Helper extension of [LevelBoundry] enum.
-extension LevelBoundryValue on LevelBoundry {
-  /// Returns string value of a enum case.
-  String get value {
-    switch (this) {
-      case LevelBoundry.country:
-        return 'country';
-        break;
-      case LevelBoundry.region:
-        return 'region';
-        break;
-      case LevelBoundry.area:
-        return 'area';
-        break;
-      case LevelBoundry.city:
-        return 'city';
-        break;
-      case LevelBoundry.settlement:
-        return 'settlement';
-        break;
-      case LevelBoundry.street:
-        return 'street';
-        break;
-      case LevelBoundry.house:
-        return 'house';
-        break;
-      default:
-        return null;
-    }
-  }
-
-  /// Converts string [value] to enum case if available.
-  /// Please check [LevelBoundry] for available cases.
-  static LevelBoundry forValue(String value) {
-    switch (value) {
-      case 'country':
-        return LevelBoundry.country;
-        break;
-      case 'region':
-        return LevelBoundry.region;
-        break;
-      case 'area':
-        return LevelBoundry.area;
-        break;
-      case 'city':
-        return LevelBoundry.city;
-        break;
-      case 'settlement':
-        return LevelBoundry.settlement;
-        break;
-      case 'street':
-        return LevelBoundry.street;
-        break;
-      case 'house':
-        return LevelBoundry.house;
-        break;
-      default:
-        return null;
-    }
-  }
+enum LevelBoundry {
+  @JsonValue('country')
+  country,
+  @JsonValue('region')
+  region,
+  @JsonValue('area')
+  area,
+  @JsonValue('city')
+  city,
+  @JsonValue('settlement')
+  settlement,
+  @JsonValue('street')
+  street,
+  @JsonValue('house')
+  house
 }
